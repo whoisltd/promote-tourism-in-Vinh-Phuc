9177da3116db5656b9fa4200a66c04e45069b7dc
WhoIsLTD <tiendat.le129@gmail.com>
2021-10-04 11:05:14 +0700

    add sqlalchemy_database_uri

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cannot-to-guess'
-    JSON_AS_ASCII = True
+    JSON_AS_ASCII = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # postgres://user:password@localhost:5432/dbname
-    
+    # clever-cloud
+    SQLALCHEMY_DATABASE_URI = 'postgres://bngy8ookxwsmy49rkk7c:ANQDOHmglqkfuvw5CP8U@bngy8ookxwsmy49rkk7c-postgresql.services.clever-cloud.com:5432/bngy8ookxwsmy49rkk7c'
