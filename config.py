8827232485654f202cd62f8ab0cb617cfc1a3740
WhoIsLTD <tiendat.le129@gmail.com>
2021-10-04 17:27:21 +0700

    change JSON_AS_ASCII to False

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'cannot-to-guess'
-    JSON_AS_ASCII = True
+    JSON_AS_ASCII = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     # postgres://user:password@localhost:5432/dbname
-    
+    SQLALCHEMY_DATABASE_URI = ''    
