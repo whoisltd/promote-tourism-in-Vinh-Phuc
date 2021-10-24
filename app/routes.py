087918cc079979b2cafc9d2dac485d8ff234b747
WhoIsLTD <tiendat.le129@gmail.com>
2021-10-24 23:29:03 +0700

    create routes.py

---
+++ app/routes.py
@@ -0,0 +1,19 @@
+from app import app
+from app.models import *
+from flask import json, render_template, request, redirect
+from flask.json import jsonify
+from sqlalchemy import func
+# from app.forms import editBuildingForm, editTreeForm
+from flask.helpers import url_for
+from flask_wtf import *
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+# from config import Config
+
+engine = create_engine('')
+da = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route('/')
+def index():
+    return render_template('services.html')
