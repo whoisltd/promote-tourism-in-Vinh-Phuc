fad47a4455b53f4fba46ea15858ae987264db8a2
vuthienson8 <thiensonvu08@gmail.com>
2021-10-25 22:06:42 +0700

    update

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 import os
 
+
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'cannot-to-guess'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "cannot-to-guess"
     JSON_AS_ASCII = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # postgres://user:password@localhost:5432/dbname
-    SQLALCHEMY_DATABASE_URI = ''    
+    SQLALCHEMY_DATABASE_URI = "postgresql://bngy8ookxwsmy49rkk7c:ANQDOHmglqkfuvw5CP8U@bngy8ookxwsmy49rkk7c-postgresql.services.clever-cloud.com:5432/bngy8ookxwsmy49rkk7c"
