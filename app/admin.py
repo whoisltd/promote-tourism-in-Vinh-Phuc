89790c4332782f4aea4a6c9103af8e7c6ecd2c37
vuthienson8 <thiensonvu08@gmail.com>
2021-11-16 16:14:06 +0700

    admin page

---
+++ app/admin.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from flask import Flask
+from app import app
+from app.models import *
+from flask_admin import Admin, BaseView, expose
+from flask_admin.contrib.sqla import ModelView
+
+class MyView(BaseView):
+    @expose('/admin/home')
+    def index(self):
+        return self.render('layout.html')
+
+admin = Admin(app=app, name= "VPhuc Travel", template_mode="bootstrap3")
+admin.add_view(ModelView(Posts, db.session))
+admin.add_view(ModelView(Services, db.session))
+admin.add_view(ModelView(Place, db.session))   
+
