b167464045a608a230b311ce5369e6c316448f3b
vuthienson8 <thiensonvu08@gmail.com>
2021-12-01 02:17:43 +0700

    update

--- app/upload_geom.py
+++ app/upload_geom.py
@@ -6,14 +6,18 @@ from sqlalchemy import func
 from app.models import *
 from flask import json, render_template, request, redirect, jsonify
 
-geojson = '{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.64662413815077, 21.455960226790893]}'
+geojson = ['{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.64472004618987, 21.456268879998778]}',
+'{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.5855035151476, 21.466358627392434]}',
+'{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.59646717616441, 21.30037309018502]}',
+'{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [21.30037309018502, 21.320349114412867]}']
 
-@app.route('/upload_geom/<int:id>', methods=['GET', 'POST'])
-def upload_geom(id):
+@app.route('/upload_geom', methods=['GET', 'POST'])
+def upload_geom():
     """
     Upload geom to database
     with EPSG:4326
     """
-    db.session.query(Tourist_area).filter(Tourist_area.id == id).update({Tourist_area.geom: func.ST_GeomFromGeoJSON(geojson)})
+    for i in range(len(geojson)):
+        db.session.query(Place).filter(Place.id == i+1).update({Place.geom: func.ST_GeomFromGeoJSON(geojson[i])})
     db.session.commit()
     return 'oke'
