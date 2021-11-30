cd362aad36208be2f4c2244bf783a14d8b8f89db
vuthienson8 <thiensonvu08@gmail.com>
2021-12-01 00:18:12 +0700

    update

---
+++ migrations/versions/e3cae8ece0c3_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: e3cae8ece0c3
+Revises: 
+Create Date: 2021-11-16 11:39:13.606506
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e3cae8ece0c3'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
