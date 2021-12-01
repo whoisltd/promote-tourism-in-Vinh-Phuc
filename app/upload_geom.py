5fe74b8981b9844a736ef3051e27ca30843389ac
WhoIsLTD <tiendat.le129@gmail.com>
2021-12-01 08:12:51 +0700

    some commit

--- app/upload_geom.py
+++ app/upload_geom.py
@@ -6,11 +6,7 @@ from sqlalchemy import func
 from app.models import *
 from flask import json, render_template, request, redirect, jsonify
 
-geojson = ['{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.64472004618987, 21.456268879998778]}',
-'{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.5855035151476, 21.466358627392434]}',
-'{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [105.59646717616441, 21.30037309018502]}',
-'{"crs":{"type": "name", "properties": {"name": "EPSG:4326"}},"type": "Point","coordinates": [21.30037309018502, 21.320349114412867]}']
-
+geojson = ['/images/places/place-1.jpg', '/images/places/place-2.jpg', '/images/places/place-3.jpg', '/images/places/place-4.jpg']
 @app.route('/upload_geom', methods=['GET', 'POST'])
 def upload_geom():
     """
@@ -18,6 +14,6 @@ def upload_geom():
     with EPSG:4326
     """
     for i in range(len(geojson)):
-        db.session.query(Place).filter(Place.id == i+1).update({Place.geom: func.ST_GeomFromGeoJSON(geojson[i])})
+        db.session.query(Place).filter(Place.id == i+1).update({Place.image: geojson[i]})
     db.session.commit()
     return 'oke'
