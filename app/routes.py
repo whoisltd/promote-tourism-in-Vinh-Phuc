7dfd147c76ffd27c8c9789ff6a5a3bce2c03ffb0
WhoIsLTD <tiendat.le129@gmail.com>
2021-12-12 22:00:59 +0700

    add marker in map to db

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from flask import render_template
+from werkzeug.utils import ArgumentValidationError
 from app import app
 from app.models import *
 from flask import json, render_template, request, redirect
@@ -20,3 +21,26 @@ da = scoped_session(sessionmaker(bind=engine))
 def index():
     return render_template("index.html")
 
+@app.route("/createNewItem", methods=["POST"])
+def createNewItem():
+    print(request.get_json())
+    data = request.get_json()
+    data["geometry"]["crs"] = {
+        "type": "name", "properties": {"name": "EPSG:4326"}}
+
+    print(data["geometry"])
+
+    result = json.dumps(data["geometry"])
+    print(result)
+
+    if data["typeMarker"] == "Service":
+        service = Services(geom=func.ST_GeomFromGeoJSON(result))
+        db.session.add(service)
+    elif data["typeMarker"] == "Area":
+        area = Tourist_area(geom=func.ST_GeomFromGeoJSON(result))
+        db.session.add(area)
+    elif data["typeMarker"] == "Place":
+        place = Place(geom=func.ST_GeomFromGeoJSON(result))
+        db.session.add(place)
+    db.session.commit()
+    return redirect(url_for('index'))
