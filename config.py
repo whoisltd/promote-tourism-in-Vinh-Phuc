f2a6fb8937ce38805ae8f8fbc0efd9df381b549c
WhoIsLTD <tiendat.le129@gmail.com>
2021-12-22 11:55:38 +0700

    update config

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ class Config:
     JSON_AS_ASCII = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     # postgres://user:password@localhost:5432/dbname
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL") or ""
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or ""
